6917803746d755cb3e45e2456f39edd8c4298a2e
Gustavo Coronel <gcoronelc@gmail.com>
2023-11-02 18:00:05 -0500

    Semana 10

---
+++ Semana10/AprendiendoHerencia/src/main/java/pe/edu/uni/aprendiendoherencia/demo1/Clase2.java
@@ -0,0 +1,11 @@
+package pe.edu.uni.aprendiendoherencia.demo1;
+
+public class Clase2 extends Clase1{
+
+	public Clase2() {
+		super("Gustavo");
+		//System.out.println("Hola");
+	}
+
+	
+}
---
+++ Semana10/AprendiendoHerencia/src/main/java/pe/edu/uni/aprendiendoherencia/demo1/Clase4.java
@@ -0,0 +1,5 @@
+package pe.edu.uni.aprendiendoherencia.demo1;
+
+public class Clase4 extends Clase2{
+
+}
